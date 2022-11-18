6fd2f028bd8cc9b6e8001afcaac7a76355d52756
Edrogarcia08@outlook.com <77702568+Edro08@users.noreply.github.com>
2022-11-17 18:34:28 -0600

    add Menu footer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:parcial04flutter/Paginas/contenedor.dart';
+import 'package:parcial04flutter/contenedor.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
